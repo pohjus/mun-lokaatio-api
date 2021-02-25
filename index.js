c4651aa9f4b011b0e70d74fbdc42063090c7b2a4
Jussi Pohjolainen <jussi.pohjolainen@gmail.com>
2021-02-25 09:44:31 +0200

    first server

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+// http://localhost:3000/taikaviitta
+app.get("/taikaviitta", (req, res) => {
+  res.send("muutos  sddssdsd");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
