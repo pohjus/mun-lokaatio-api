6fe78c55f90a6a1cce647ed4f3fdc441cf0049eb
Jussi Pohjolainen <jussi.pohjolainen@gmail.com>
2021-02-25 09:51:09 +0200

    comment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 const port = process.env.PORT || 8080;
-
+// test
 app.use(express.static("public"));
 
 // http://localhost:3000/taikaviitta
